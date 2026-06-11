b20acc8ccf0689130fab522481d62b860eaf41f0
pablo-dev989 <261613295+pablo-dev989@users.noreply.github.com>
2026-06-11 16:37:38 -0400

    relacion uno a muchos en django

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,16 @@
 # django
 from django.urls import path
+from . import views
 
 # music views
-from .views import IndexView
+#from .views import IndexView
 
+
+#urlpatterns = [
+#    path('', IndexView.as_view()),
+#]
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path("", views.index, name='index'),
+    path("artist/<artist_id>/", views.artist_detail, name='artist_detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,18 @@
 # django
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
-class IndexView(TemplateView):
-    template_name = "music/index.html"
+from music.models import Artist
+
+
+#class IndexView(TemplateView):
+#    template_name = "music/index.html"
+
+def index(request):
+    return HttpResponse("Bienvenido a la app de Music.")
+
+def artist_detail(request, artist_id):
+    artist = Artist.objects.get(id=artist_id)
+    context = {'artist': artist}
+    return render(request, 'music/artist_detail.html', context)
